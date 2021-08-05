05dfda7b2b1c2216c81864ec152ca394fc19f015
Jaco Gillman <gillmanjc@gmail.com>
2021-08-05 15:32:34 +0200

    Updated GbAssignmentData

--- gradebookng/tool/src/java/org/sakaiproject/gradebookng/tool/model/GbAssignmentData.java
+++ gradebookng/tool/src/java/org/sakaiproject/gradebookng/tool/model/GbAssignmentData.java
@@ -5,16 +5,18 @@ import java.util.List;
 
 import org.apache.wicket.util.io.IClusterable;
 
-import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.ToString;
 
 /**
  * GbAssignmentData
  *
  * @author Joseph Gillman
  */
-@Data
+@ToString
+@EqualsAndHashCode
 public class GbAssignmentData implements IClusterable {
-	
+
 	private static final long serialVersionUID = 1L;
 
 	private boolean selected;
@@ -22,6 +24,33 @@ public class GbAssignmentData implements IClusterable {
 	private String assignmentId;
 
 	private String assignmentName;
-	
+
 	private List<GbStudentInfoData> studentInfoDataList = new ArrayList<GbStudentInfoData>();
+
+	public GbAssignmentData() {
+	}
+
+	public String getAssignmentId() {
+		return assignmentId;
+	}
+
+	public void setAssignmentId(String assignmentId) {
+		this.assignmentId = assignmentId;
+	}
+
+	public String getAssignmentName() {
+		return assignmentName;
+	}
+
+	public void setAssignmentName(String assignmentName) {
+		this.assignmentName = assignmentName;
+	}
+
+	public List<GbStudentInfoData> getStudentInfoDataList() {
+		return studentInfoDataList;
+	}
+
+	public void setStudentInfoDataList(List<GbStudentInfoData> studentInfoDataList) {
+		this.studentInfoDataList = studentInfoDataList;
+	}
 }

8d228d659e1931e6229f00cbff2042e10d74ff97
Jaco Gillman <gillmanjc@gmail.com>
2021-10-19 14:35:56 +0200

    Updated gradebook-nwu-mps.js

--- gradebookng/tool/src/webapp/scripts/gradebook-nwu-mps.js
+++ gradebookng/tool/src/webapp/scripts/gradebook-nwu-mps.js
@@ -5,13 +5,13 @@
 function selectAllCheckboxes() {
 	if(document.getElementById('select-all').checked) { //selecting all
 	    $(':checkbox').each(function() {
-	      if(this.name.includes('select-all') && !this.checked && !this.disabled) {
+	      if(!this.name.includes('select-all') && !this.checked && !this.disabled) {
 	        this.click();
 	      }
 	    });
 	} else {  //deselecting all
 	    $(':checkbox').each(function() {
-	      if(this.name.includes('select-all') && this.checked && !this.disabled) {
+	      if(!this.name.includes('select-all') && this.checked && !this.disabled) {
 	        this.click();
 	      }
 	    });
